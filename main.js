860f11794397311aaf4d092e42605c8e52f2cecf
AndreaCarolinaBaronCaceres <acbaca6789@gmail.com>
2021-07-10 22:14:30 -0500

    Sintaxis_JavaScript_02_07_2021

---
+++ main.js
@@ -0,0 +1,31 @@
+
+console.log("Conexion Exitosa");
+
+num1 = 2 ;
+num2 = 3;
+suma = num1 + num2;
+console.log(suma); 
+console.log(`El resultado de la suma es: ${suma}`);
+
+
+num1 = 50;
+num  = 3;
+// tipoDeDato = typeof(num1);
+
+// console.log(tipoDeDato);
+
+
+
+nombreCompleto = null;
+
+console.log(typeof(nombreCompleto))
+nombreString = `Andrea Carolina Baron Caceres `;
+edadNumber = 21;
+soyProfesorBoolean = false;
+estudiantesAprobadosDiplomadoNull = null;
+cocianarUndefined = undefined;
+hobbiesArray = ["programar","cocinar",null];
+direccionPersonalObjeto =  {
+    direccion:"calle 115"
+};      
+console.log(hobbiesArray)
